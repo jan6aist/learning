35118890ed5447fe7a3ec7d859cf22e7ba1b57ac
jan6aist <144102969+jan6aist@users.noreply.github.com>
2023-09-05 13:26:03 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+document.write("Javascript говорит привет"); /* вывод информации */         /* */
+console.log("Javascript говорит привет"); /* вывод информации в консоль */
+console.error("Javascript говорит привет") /* вывод ошибки .warn -предупреждение */
+
+var num; /*переменная*/
+num = 5; /* либо записываем var num = 5; */
+console.log("Переменная:" + num);
